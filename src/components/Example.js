00eba355b075ce98caa699b9766c7bddfd61b061
jamin-coo <jamin@cooljamm.com>
2020-08-05 22:28:48 +0900

    Update Example.js

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,26 +1,18 @@
-import React, {useState} from 'react';
-import 'react-phone-number-input/style.css'
-import { getCountries, getCountryCallingCode } from 'react-phone-number-input/input'
-import en from 'react-phone-number-input/locale/en.json'
-
+import React, { useState } from 'react';
+import 'react-phone-number-input/style.css';
+import { getCountries, getCountryCallingCode } from 'react-phone-number-input/input';
+import en from 'react-phone-number-input/locale/en.json';
 
 export default function Example() {
-  const [country, setCountry] = useState()
+  const [country, setCountry] = useState();
   return (
-<select
-  value={country}
-  onChange={event => setCountry(event.target.value || undefined)}>
-  <option value="">
-    {en['ZZ']}
-  </option>
-  {getCountries().map((country) => (
-    <option key={country} value={country}>
-      {en[country]} +{getCountryCallingCode(country)}
-    </option>
-  ))}
-</select>
-  )
+    <select value={country} onChange={(event) => setCountry(event.target.value || undefined)}>
+      <option value=''>{en['KR']}</option>
+      {getCountries().map((country) => (
+        <option key={country} value={country}>
+          {en[country]} +{getCountryCallingCode(country)}
+        </option>
+      ))}
+    </select>
+  );
 }
-
-
-
